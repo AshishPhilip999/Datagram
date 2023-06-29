60ac2504d10566f0a475e33c71e6ceef8a9feb8c
AshishPhilip999 <ashishphilip186@gmail.com>
2023-06-29 14:04:02 +0530

    added button func

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,32 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>DataGram...</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  const [count , setCount] = useState(2);
+  const [buttonlist , setButtons] = useState([]);
+
+  function onButtonClick(e){
+
+    setButtons( buttons =>{
+      return (
+        [...buttons , { value : ("Button" + count ) , id : crypto.randomUUID() }]
+      )
+    } )
+  }
+  
+  
+  return <>
+  <div className='newbutton'>
+  <ul id='list'>
+  <button className='table-button'  onClick={e => { setCount(count+1); onButtonClick(e) }} >Button</button>
+    {buttonlist.map(
+      lists =>{
+        return <button className='table-button'>{lists.value}</button>
+      }
+    )}
+  </ul>
+  </div>
+  </>
 }
 
 export default App
